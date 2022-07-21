8bdf1b760a9606809ef9339441806e162c16fbee
Nader Ebrahim~ <nebrahim2424@gmail.com>
2022-07-21 10:20:31 -0700

    downloaded fonts to improve load times. ran build

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -24,20 +24,7 @@ const EmailItem = styled(motion.div)`
   :hover {
     opacity: 1;
   }
-  @font-face {
-    font-family: "Bahnschrift";
-    src: url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.eot");
-    src: url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.eot?#iefix")
-        format("embedded-opentype"),
-      url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.woff2")
-        format("woff2"),
-      url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.woff")
-        format("woff"),
-      url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.ttf")
-        format("truetype"),
-      url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.svg#Bahnschrift")
-        format("svg");
-  }
+
   font-family: Bahnschrift;
   color: white;
   font-size: 18px;
--- src/components/Sections/Homepage.tsx
+++ src/components/Sections/Homepage.tsx
@@ -43,20 +43,6 @@ const WelcomeTextContainer = styled.div`
 const WelcomeText = styled(motion.h1)`
   display: flex;
   flex-direction: column;
-  @font-face {
-    font-family: "Bahnschrift";
-    src: url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.eot");
-    src: url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.eot?#iefix")
-        format("embedded-opentype"),
-      url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.woff2")
-        format("woff2"),
-      url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.woff")
-        format("woff"),
-      url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.ttf")
-        format("truetype"),
-      url("//db.onlinewebfonts.com/t/0a6ee448d1bd65c56f6cf256a7c6f20a.svg#Bahnschrift")
-        format("svg");
-  }
   font-family: Bahnschrift;
   color: white;
   font-size: 50px;
